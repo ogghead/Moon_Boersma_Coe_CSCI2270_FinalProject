01be6cde4f5e2ff0d30b64ca48f1f294cea298c9
alco3419 <alco3419@colorado.edu>
2016-04-21 16:07:52 -0600

    build deck and shuffle deck functions

--- Blackjack.cpp
+++ Blackjack.cpp
@@ -1,60 +1,99 @@
-#ifndef BLACKJACK_H
-#define BLACKJACK_H
-#include <string>
+#include "Blackjack.h"
+#include <stdlib.h>
+#include <iostream>
+#include <algorithm>
 #include <vector>
-struct card
+#include <ctime>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+Blackjack::Blackjack()
 {
-    int value;
-    int suit;
-    std::string name;
-    int counter;
-};
+    //ctor
 
-struct player
+}
+
+Blackjack::~Blackjack()
 {
-    std::string name;
-    int orientation;//1 defensive, 2 normal, 3 aggressive playstyle
-    int handValue;
-    std::vector<card> hand;
-    int totalMoney;
-    int bet;
-    bool isSplit;
-    int handValue2 = 0;
-    std::vector<card> hand2;
-    int bet2 = 0;
-    bool stand = false;
-};
+    //dtor
+}
 
-struct dealer
-{
-    int handValue;
-    std::vector<card> hand;
-    bool stand = false;
-};
+void Blackjack::buildDeck(int deckNumber){
+    for (int s = 0; s < deckNumber; s++){
+        for(int k=0;k<52;k++){
+            for(int i=0;i<4;i++){
+                for(int j=0; j<13;j++){
+                    card *newCard = new card;
+                    newCard->suit=i;
 
-class Blackjack
-{
-private:
-dealer Dealer;
-std::vector<card*> deck;
-std::vector<player> players;
-int cardCounter;
+                    if (j+1 == 1){
+                        newCard->value = 11;
+                        newCard->name = "Ace";
+                    }
+                    else if (j+1 == 2){
+                        newCard->value = j + 1;
+                        newCard->name = "Two";
+                    }
+                    else if (j+1 == 3){
+                        newCard->value = j + 1;
+                        newCard->name = "Three";
+                    }
+                    else if (j+1 == 4){
+                        newCard->value = j + 1;
+                        newCard->name = "Four";
+                    }
+                    else if (j+1 == 5){
+                        newCard->value = j + 1;
+                        newCard->name = "Five";
+                    }
+                    else if (j+1 == 6){
+                        newCard->value = j + 1;
+                        newCard->name = "Six";
+                    }
+                    else if (j+1 == 7){
+                        newCard->value = j + 1;
+                        newCard->name = "Seven";
+                    }
+                    else if (j+1 == 8){
+                        newCard->value = j + 1;
+                        newCard->name = "Eight";
+                    }
+                    else if (j+1 == 9){
+                        newCard->value = j + 1;
+                        newCard->name = "Nine";
+                    }
+                    else if (j+1 == 10){
+                        newCard->value = j + 1;
+                        newCard->name = "Ten";
+                    }
+                    else if (j+1 == 11){
+                        newCard->value = 10;
+                        newCard->name = "Jack";
+                    }
+                    else if (j+1 == 12){
+                        newCard->value = 10;
+                        newCard->name = "Queen";
+                    }
+                    else if (j+1 == 13){
+                        newCard->value = 10;
+                        newCard->name = "King";
+                    }
+                    //cout<<newCard->name<<endl;
+                    deck.push_back(newCard);
+                }
+            }
+        }
+    }
+}
 
-public:
-    Blackjack();
-    ~Blackjack();
-    void buildDeck(int deckNumber);//random number of 52 card decks shuffled
-    void shuffleDeck(int deckNumber);
-    void createPlayers(int playerNumber);//random orientation players created
-    void Deal();
-    void HitMe();
-    void Stand();
-    void Split();
-    void DoubleDown();
-    void addCardCount(int counter);
-    int displayCardCounter();
-    void newRound();
-};
+void Blackjack::shuffleDeck(int deckNumber){
+    srand(time(NULL));
+    random_shuffle(deck.begin(),deck.end());
+    cout<<deck[0]->name << endl;
+    cout<<deck[20]->name << endl;
+    cout<< deck[51]->name<<endl;
+    //deck.pop_back();
+    cout<<(deck.size())-(2652*deckNumber)<<endl;
+}
 
-
-#endif

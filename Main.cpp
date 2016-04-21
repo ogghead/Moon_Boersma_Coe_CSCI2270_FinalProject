f85048903f4ef955ba4314829dfe15c9553d3b7a
alco3419 <alco3419@colorado.edu>
2016-04-21 16:09:50 -0600

    Update Main.cpp
    testing purposes for shuffle and build deck

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,16 @@
-cout<<"hello world"<<endl;
+#include <iostream>
+#include "Blackjack.h"
+#include <stack>
+#include <algorithm>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+using namespace std;
+
+int main()
+{
+    Blackjack myGame;
+    myGame.buildDeck(1);
+    myGame.shuffleDeck(1);
+    return 0;
+}

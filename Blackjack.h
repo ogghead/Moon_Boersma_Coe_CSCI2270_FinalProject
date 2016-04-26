a030d0092cadf091574504616c541d94355599b6
RichardWasTaken <rimo1046@colorado.edu>
2016-04-26 14:52:37 -0600

    Update Blackjack.h

--- Blackjack.h
+++ Blackjack.h
@@ -5,7 +5,7 @@
 struct card
 {
     int value;
-    std::string suit;
+    int suit;
     std::string name;
     int counter;
 };
@@ -15,14 +15,15 @@ struct player
     std::string name;
     int orientation;//1 defensive, 2 normal, 3 aggressive playstyle
     int handValue;
-    std::vector<card> hand;
+    std::vector<card*> hand;
     int totalMoney;
     int bet;
     bool isSplit;
     int handValue2 = 0;
-    std::vector<card> hand2;
+    std::vector<card*> hand2;
     int bet2 = 0;
     bool stand = false;
+    bool stand2 = false;
 };
 
 struct dealer
@@ -35,24 +36,27 @@ struct dealer
 class Blackjack
 {
 private:
-dealer Dealer;
-std::vector<card> deck;
-std::vector<player> players;
-int cardCounter;
+    dealer Dealer;
+    std::vector<card*> deck;
+    std::vector<player*> players;
+    int cardCounter;
 
 public:
     Blackjack();
     ~Blackjack();
     void buildDeck(int deckNumber);//random number of 52 card decks shuffled
+    void shuffleDeck(int deckNumber);
     void createPlayers(int playerNumber);//random orientation players created
     void Deal();
-    void HitMe();
-    void Stand();
+    void HitMe(player* currentPlayer, bool secondHand);
+    void Stand(player* player);
     void Split();
     void DoubleDown();
     void addCardCount(int counter);
     int displayCardCounter();
     void newRound();
+    bool ai(player*);
+    std::vector <player*> returnPlayers();
 };
 
 

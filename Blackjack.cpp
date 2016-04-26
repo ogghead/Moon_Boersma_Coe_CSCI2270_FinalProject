c915a4cc44eb5af317cfde8edbbf6de00e4e1444
RichardWasTaken <rimo1046@colorado.edu>
2016-04-26 14:51:25 -0600

    Update Blackjack.cpp

--- Blackjack.cpp
+++ Blackjack.cpp
@@ -1,1 +1,186 @@
 #include "Blackjack.h"
+#include <stdlib.h>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <ctime>
+#include <cstdlib>
+#include <string>
+using namespace std;
+
+Blackjack::Blackjack()
+{
+    //ctor
+
+}
+
+Blackjack::~Blackjack()
+{
+    //dtor
+}
+
+void Blackjack::buildDeck(int deckNumber){
+    for (int s = 0; s < deckNumber; s++){
+
+        for(int i=0;i<4;i++){
+            for(int j=0; j<13;j++){
+                card *newCard = new card;
+                newCard->suit=i;
+                if (j+1 == 1){
+                    newCard->value = 11;
+                    newCard->name = "Ace";
+                }
+                    else if (j+1 == 2){
+                        newCard->value = j + 1;
+                        newCard->name = "Two";
+                    }
+                    else if (j+1 == 3){
+                        newCard->value = j + 1;
+                        newCard->name = "Three";
+                    }
+                    else if (j+1 == 4){
+                        newCard->value = j + 1;
+                        newCard->name = "Four";
+                    }
+                    else if (j+1 == 5){
+                        newCard->value = j + 1;
+                        newCard->name = "Five";
+                    }
+                    else if (j+1 == 6){
+                        newCard->value = j + 1;
+                        newCard->name = "Six";
+                    }
+                    else if (j+1 == 7){
+                        newCard->value = j + 1;
+                        newCard->name = "Seven";
+                    }
+                    else if (j+1 == 8){
+                        newCard->value = j + 1;
+                        newCard->name = "Eight";
+                    }
+                    else if (j+1 == 9){
+                        newCard->value = j + 1;
+                        newCard->name = "Nine";
+                    }
+                    else if (j+1 == 10){
+                        newCard->value = j + 1;
+                        newCard->name = "Ten";
+                    }
+                    else if (j+1 == 11){
+                        newCard->value = 10;
+                        newCard->name = "Jack";
+                    }
+                    else if (j+1 == 12){
+                        newCard->value = 10;
+                        newCard->name = "Queen";
+                    }
+                    else if (j+1 == 13){
+                        newCard->value = 10;
+                        newCard->name = "King";
+                    }
+                    //cout<<newCard->name<<endl;
+                    deck.push_back(newCard);
+                }
+            }
+        }
+    }
+
+
+void Blackjack::shuffleDeck(int deckNumber){
+    srand(time(NULL));
+    random_shuffle(deck.begin(),deck.end());
+
+}
+
+void Blackjack::HitMe(player* currentPlayer, bool secondHand)//this boolean checks
+//if the hand to deal to is the 2nd one, if a player has split
+{
+    int index = deck.size() - 1;
+    card* tmp = deck[index];
+
+    if (secondHand == false)
+    {
+        currentPlayer->hand.push_back(tmp);
+    }
+
+    else
+    {
+        currentPlayer->hand2.push_back(tmp);
+    }
+    deck.pop_back();
+    delete tmp;
+}
+
+void Blackjack::Stand(player* player)
+{
+    player->stand=true;
+}
+
+
+bool Blackjack::ai(player * player){
+    int ori=player->orientation;
+        while(player->stand==false){
+            if(ori==0){
+                break;
+            }
+
+            else if(ori==2){
+                if(player->handValue < 17){
+                    HitMe(player,false);
+                }
+            if(player->handValue >= 17){
+                Stand(player);
+                }
+            }
+            else if(ori==1){
+                if(player->handValue < 16){
+                    HitMe(player,false);
+                }
+                if(player->handValue >= 16){
+                    Stand(player);
+                }
+            }
+
+            else if(ori==3){
+                if(player->handValue < 18){
+                    HitMe(player,false);
+                }
+                if(player->handValue >= 18){
+                    Stand(player);
+                }
+            }
+        }
+    }
+
+void Blackjack::createPlayers(int playerNumber)
+{
+    player* me = new player;
+    me->orientation = 0;
+    me->totalMoney = 1000;
+
+    players.push_back(me);
+
+    srand(time(NULL));
+
+    for(int i = 1; i < playerNumber; i++)
+    {
+        player* newAI = new player;
+        newAI->orientation = rand() % 4 + 1;
+        newAI->totalMoney = 1000;
+        players.push_back(newAI);
+    }
+}
+
+vector <player*> Blackjack::returnPlayers()
+{
+    return players;
+}
+
+void Blackjack::Deal()
+{
+    for (int i = 0; i < players.size(); i++)
+    {
+        HitMe(players[i], false);
+        HitMe(players[i], false);
+    }
+}
